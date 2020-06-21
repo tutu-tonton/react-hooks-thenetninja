006169e84131f6e0930742e35950b4265e54da28
tsutomu-noguchi <50165379+tsutomu-noguchi@users.noreply.github.com>
2020-06-21 15:56:59 +0900

    lesson11. useState with Forms

---
+++ src/components/NewSongForm.component.jsx
@@ -0,0 +1,26 @@
+//========================================
+//  入力欄コンポーネント
+//  ローカルstate所持
+//  親からのメソッドに、ローカルstateを詰め込んで、親に返す
+//========================================
+
+import React, { useState } from 'react';
+
+const NewSongForm = ({ addSong }) => {
+	const [title, setTitle] = useState('');
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		// console.log(title);
+		addSong(title);
+		setTitle('');
+	};
+	return (
+		<form onSubmit={handleSubmit}>
+			<label>Song name:</label>
+			<input type="text" value={title} required onChange={(e) => setTitle(e.target.value)} />
+			<input type="submit" value="add song" />
+		</form>
+	);
+};
+
+export default NewSongForm;
